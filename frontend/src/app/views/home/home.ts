22e94a4f416416ee34cc5cb8d5968c43e9b0012c
evavranici <evavranici@gmail.com>
2025-07-05 15:11:43 +0200

    add home page

--- frontend/src/app/views/home/home.ts
+++ frontend/src/app/views/home/home.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Player } from '../../shared/interfaces/player';
+import { PlayerService } from '../../shared/services/player-service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrls: ['./home.css'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+  ]
 })
-export class Home {
+export class Home implements OnInit {
+  players: Player[] = [];
+  selectedPlayer: Player | null = null;
 
+  constructor(private router: Router, private playerService: PlayerService) { }
+
+  ngOnInit(): void {
+    this.fetchPlayers();
+  }
+
+  fetchPlayers(): void {
+    this.playerService.getAllPlayers().subscribe({
+      next: (data) => { this.players = data; },
+      error: (error) => { console.error('Error fetching players:', error); }
+    });
+  }
+
+  selectPlayer(player: Player): void {
+    this.selectedPlayer = player;
+    this.router.navigate(['/rps-play', this.selectedPlayer.id]);
+  }
+
+  goToCreatePlayer(): void {
+    this.router.navigate(['/add-player']);
+  }
+
+  viewLeaderboard(): void {
+    this.router.navigate(['/leaderboard']);
+  }
 }
